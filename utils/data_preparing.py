6f48293183fb5e1ad4188b102cbd01ce7e6431b6
Akechi1412 <nguyenphong10042002@gmail.com>
2024-04-18 13:22:51 +0700

    Fix something in data_preparing

--- utils/data_preparing.py
+++ utils/data_preparing.py
@@ -2,6 +2,7 @@ import csv
 from .data_prepropressing import preprocessing
 
 def prepare_data():
+    print("Preparing data...")
     x_data = []
     y_data = []
     with open('data/reviews_data.csv', 'r', encoding='utf-8') as file:
@@ -9,8 +10,9 @@ def prepare_data():
         
         for row in reader:
             word_list = preprocessing(row[0])
+            label = int(row[1])
             if len(word_list) > 0:
                 x_data.append(word_list)
-            y_data.append(int(row[1]))
+                y_data.append(label)
     
     return x_data, y_data

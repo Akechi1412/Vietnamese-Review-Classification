ce570645d7e65df056bc85b45019bf37bae82437
Đăng Thức Nguyễn <thuc.nguyendang@hcmut.edu.vn>
2024-04-18 23:09:51 +0700

    add new code

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
 def predict_sentiment(text):
-    model = load_model('/models/review_model.keras')
+    model = tf.keras.models.load_model('D:/Vietnamese-Review-Classification/models/review_model.keras')
 
     tokenizer = None
-    with open("/models/tokenizer.pkl", "rb") as f:
+    with open("D:/Vietnamese-Review-Classification/models/review_tokenizer.pkl", "rb") as f:
         tokenizer = pickle.load(f)
-    
-    max_len=200
+    max_len = 200
     sequence = tokenizer.texts_to_sequences([text])
     padded_sequence = pad_sequences(sequence, maxlen=max_len)
 
-    prediction = model.predict_classes(padded_sequence)[0][0]
+    prediction = model.predict(padded_sequence)
+    predicted_class = tf.argmax(prediction, axis=-1).numpy()[0]
 
-    print(prediction)
+    print(predicted_class)
 
-predict_sentiment('Sản phẩm bình thường')
+predict_sentiment('sản phẩm như cái lồn')
 
